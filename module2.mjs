33b3da6bafec3e03d92b95c26fd001d5597553d8
Kimberley Thill <ThillKimberleyPro@gmail.com>
2024-09-24 11:29:48 +0200

    module2 klaar

---
+++ module2.mjs
@@ -0,0 +1,10 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let geboorteJaar = await userInput.question('Wat is je geboorteJaar? ');
+console.log(geboorteJaar);
+
+let toekomstJaar = 2031 - geboorteJaar;
+
+console.log("In 2031 zult u " + toekomstJaar + "jaar zijn");
